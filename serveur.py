dde8158ec8b592b0553f7aed680048f6c13a3af6
Tsipora Enkaoua <tsipoenkaoua@gmail.com>
2025-08-24 19:24:58 +0200

    ajout des pages HTML

--- serveur.py
+++ serveur.py
@@ -1,38 +1,30 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from urllib.parse import parse_qs #pour parser
+import html  # pour sécuriser l’affichage et empecher les failles XSS, scripts ds les champsde formiulaires
 
 
 class SimpleHandler(BaseHTTPRequestHandler):#SimpleHandler hérite de BaseHTTPRequestHandler
     def do_GET(self):
-      if(self.path == "/"):
+      if(self.path == "/home"):
         self.send_response(200)  # self correspond a l'objet courant, une instance de SimpleHandler générée directement des qu'une requete est envoyée (post ou get). Cette instance comporte les infos de la requete et va pouvoir executer la méthode correspondante et renvoyer ce qui est demandé
         self.send_header('Content-type', 'text/html')  
         self.end_headers()  # fin des headers
-        self.wfile.write(b"Bienvenue a la page d'accueil !")  # corps de la réponse
+        with open("templates/home.html", "rb") as f:
+          self.wfile.write(f.read())
        
       elif(self.path == "/about"):
         self.send_response(200)  
         self.send_header('Content-type', 'text/html')  
         self.end_headers() 
-        self.wfile.write(b"Bienvenue a la page d'a propos !")
+        with open("templates/about.html", "rb") as f:
+          self.wfile.write(f.read())
        
       elif(self.path == "/contact"):
         self.send_response(200)  
         self.send_header('Content-type', 'text/html')  
         self.end_headers()
-        html="""
-        <h2>Formulaire de contact</h2>
-        <form method = 'POST', action="/contact">
-        <label for="fname">First name:</label><br>
-        <input type="text" id="fname" name="fname" value=""><br>
-        <label for="lname">Last name:</label><br>
-        <input type="text" id="lname" name="lname" value=""><br>
-        <label for="message">Description</label><br>
-        <textarea name="message"></textarea><br>
-        <input type="submit" value="Submit">
-        </form>
-        """
-        self.wfile.write(html.encode())
+        with open("templates/contact.html", "rb") as f:
+          self.wfile.write(f.read())
        
       else:
         self.send_response(404)  
@@ -41,7 +33,7 @@ class SimpleHandler(BaseHTTPRequestHandler):#SimpleHandler hérite de BaseHTTPRe
         self.wfile.write(b"Erreur, la page que vous recherchez n'existe pas !")
         
     def do_POST(self):
-      if(self.path == "/contact"):
+      if(self.path == "/envoi_contact"):
         content_length = int(self.headers['Content-Length'])
         body = self.rfile.read(content_length)  # ça te donne les données en bytes
         data = body.decode('utf-8')
@@ -54,18 +46,19 @@ class SimpleHandler(BaseHTTPRequestHandler):#SimpleHandler hérite de BaseHTTPRe
         self.send_response(200)
         self.send_header('Content-type', 'text/html; charset=utf-8')
         self.end_headers()
-        html= f"""
-        <h2>Merci {firstname}</h2>
-        <h3>Votre message a bien été enregistré<br>
-        <a href="/">Retourner à la page d'accueil</a>
-        </h3>
-        """
-        self.wfile.write(html.encode())
+        # Lire le HTML comme texte
+        with open("templates/envoi_contact.html", "r", encoding="utf-8") as f:
+          html_template = f.read()
+        # Remplacer le placeholder par la valeur sécurisée
+        safe_firstname = html.escape(firstname)
+        html_response = html_template.replace("{{firstname}}", safe_firstname)
+        # Envoyer au navigateur
+        self.wfile.write(html_response.encode("utf-8"))
   
 
 # 2️⃣ Créer et lancer le serveur
 host = 'localhost'
 port = 8000
 server = HTTPServer((host, port), SimpleHandler)
-print(f"Serveur en écoute sur http://{host}:{port}/")
+print(f"Serveur en écoute sur http://{host}:{port}/home")
 server.serve_forever() # garde le serveur actif
